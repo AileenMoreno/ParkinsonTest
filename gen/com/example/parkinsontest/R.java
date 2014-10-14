95a4c04b547380bcc2c4927c623a9f386383d7c0
Aileen-Moreno <aileenmoreno@hotmail.com>
2014-10-14 16:30:53 -0430

    Faster Button y Splash_Screen_v1

--- gen/com/example/parkinsontest/R.java
+++ gen/com/example/parkinsontest/R.java
@@ -25,14 +25,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int logo=0x7f020001;
         public static final int splash=0x7f020002;
+        public static final int titulo_blanco=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
+        public static final int action_settings=0x7f090002;
         public static final int container=0x7f090000;
+        public static final int imgLogo=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_splash=0x7f030001;
+        public static final int fragment_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
